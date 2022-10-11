37af3821ef15a187b5ddcde8f4bf0d83a8b07ee7
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-10-11 19:58:42 +0200

    installed react router and added the different pages under a pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { FC } from "react";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
-const App = () => {
-  return <h1> Welcome to the realtor project</h1>;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App: FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/Offers.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Offers: FC = () => {
+  return <div>Offers</div>;
+};
+
+export default Offers;
