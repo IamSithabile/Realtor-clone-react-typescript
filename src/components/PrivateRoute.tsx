41d3070f2e2bdc3faa9fa8439df0fa97cdd05f94
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-10-15 18:49:43 +0200

    Created a spinner component

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Outlet } from "react-router";
 import SignIn from "../pages/SignIn";
 import useAuthStatus from "../Hooks/useAuthStatus";
+import Spinner from "./Spinner";
 
 const PrivateRoute = (): JSX.Element => {
   const { isLoggedIn, isChecking } = useAuthStatus();
 
   if (isChecking) {
-    return <h3 className="text-3xl font-bold text-center mt-12">Loading...</h3>;
+    return <Spinner />;
   }
 
   //
