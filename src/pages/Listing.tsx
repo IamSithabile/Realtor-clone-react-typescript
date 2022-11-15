f74385597c1c65cd3f4d3b1f3f7b873a97e68901
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-11-15 18:20:43 +0200

    worked on the listing page, display listing info

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -15,7 +15,14 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/css/bundle";
 
-import { FaShare } from "react-icons/fa";
+import {
+  FaShare,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaParking,
+  FaChair,
+} from "react-icons/fa";
 
 type Props = {};
 
@@ -26,7 +33,7 @@ const Listing = (props: Props) => {
 
   const params = useParams();
 
-  const { category, categoryName, listingId } = params;
+  const { listingId } = params;
 
   SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -78,7 +85,7 @@ const Listing = (props: Props) => {
       </Swiper>
       <div className="absolute top-[13%] right-[3%] z-50 bg-white rounded-full border-gray-300 border-2 cursor-pointer">
         <FaShare
-          className=" flex text-4xl justify-center align-center text-slate-500 p-2"
+          className=" flex text-4xl justify-center align-center text-slate-500 p-2 "
           onClick={() => {
             navigator.clipboard.writeText(window.location.href);
             setCopiedText(true);
@@ -90,10 +97,62 @@ const Listing = (props: Props) => {
         />
       </div>
       {copiedText && (
-        <p className="absolute top-[18%] right-[3%] z-10 bg-white font-semibold p-1 rounded">
-          Text Copied!
+        <p className="absolute top-[18%] right-[3%] z-10 bg-white font-semibold p-1 rounded-md border-gray-300 border-2">
+          Link Copied!
         </p>
       )}
+
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+        <div className=" w-full ">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - ${" "}
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" ? " / month" : ""}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold">
+            <FaMapMarkerAlt className="text-green-700 mr-1" />
+            {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === "rent" ? "Rent" : "Sale"}
+            </p>
+            {listing.offer && (
+              <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
+                ${+listing.regularPrice - +listing.discountedPrice} discount
+              </p>
+            )}
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold">Description - </span>
+            {listing.description}
+          </p>
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6">
+            <li className="flex items-center whitespace-nowrap">
+              <FaBed className="text-lg mr-1" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaParking className="text-lg mr-1" />
+              {listing.parking ? "Parking spot" : "No parking"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaChair className="text-lg mr-1" />
+              {listing.furnished ? "Furnished" : "Not furnished"}
+            </li>
+          </ul>
+        </div>
+      </div>
     </main>
   );
 };
